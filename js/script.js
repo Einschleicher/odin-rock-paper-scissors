0cb43848d85ce36a67a70e4ddbacac8579c73003
Einschleicher <116537717+Einschleicher@users.noreply.github.com>
2023-05-27 21:34:53 +0200

    Add playRound function

--- js/script.js
+++ js/script.js
@@ -11,5 +11,35 @@ function getComputerChoice() {
     }
 }
 
+function playRound(playerSelection, computerSelection) {
+    
+    playerSelection = playerSelection.toLowerCase();
+    
+    if (playerSelection === computerSelection) {
+        return `Tie! Both of you did choose ${playerSelection}`
+    }
+    else if (playerSelection === "paper" && computerSelection === "rock") {
+        return "You win! Paper beats rock";
+    }
+    else if (playerSelection === "paper" && computerSelection === "scissors") {
+        return "You lose! Scissors beats paper";
+    }
+    else if (playerSelection === "rock" && computerSelection === "paper") {
+        return "You lose! Paper beats rock";
+    }
+    else if (playerSelection === "rock" && computerSelection === "scissors") {
+        return "You win! Rock beats scissors";
+    }
+    else if (playerSelection === "scissors" && computerSelection === "paper") {
+        return "You win! Scissors beats paper";
+    }
+    else if (playerSelection === "scissors" && computerSelection === "rock") {
+        return "You lose! Rock beats Scissors";
+    }
+}
+
 // Code testing
-console.log(getComputerChoice());
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+console.log(computerSelection);
+console.log(playRound(playerSelection, computerSelection));
